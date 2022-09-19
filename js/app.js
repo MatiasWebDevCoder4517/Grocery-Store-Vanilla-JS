49cb11a12bd4553530c48e1c5c466d3b59af6c2f
Matias Gonzalez <mgonzalez@qin.cl>
2022-09-19 20:38:34 -0300

    add-styles-pre-js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+"use strict"
+
+// ****** SELECT ITEMS **********
+
+
+// edit option
+
+// ****** event listeners **********
+
+// submit form
+
+// clear list
+
+// display items onload
+
+
+// ****** functions **********
+
+// add item
+
+// display alert
+
+
+// clear items
+
+
+// delete item
+
+
+// edit item
+
+
+// ****** local storage **********
+
+// add to local storage
+
+
+
+// ****** setup items **********
+
+
+
